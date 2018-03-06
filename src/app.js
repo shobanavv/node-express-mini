20ca312d45546e733d347d1d4afbe905f48ecc91
shobanavv <shobanavv@gmail.com>
2018-03-06 10:05:40 -0800

    Tried get and post, works for simple req and res. Following Ivan's model solution to work with hangman game

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const fs = require('fs');
 
 const STATUS_USER_ERROR = 422;
+const PORT = 3000;
+let wordArr = [];
+let wordSoFar = [];
+const finalWord =[];
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -13,7 +17,51 @@ const readWords = () => {
   const contents = fs.readFileSync('words.txt', 'utf8');
   return contents.split('\n');
 };
+const word = readWords()[100];
 
-// TODO: your code to handle requests
+server.get("/", (req, res) => {
+    
+    console.log(finalWord);
+    let currentWord =word.split(' ');
+    currentWord = currentWord.map(l => {
 
-server.listen(3000);
+    })
+    res.status(200);
+    res.send(finalWord);
+});
+
+server.post("/guess", (req, res) => {
+    // if(wordArr.length === 0) {
+    //     finalWord.forEach(l => {
+    //        wordSoFar.push('-');
+    //     });
+    // }
+    // console.log(wordSoFar);
+    // res.status(200);
+    // res.send(wordSoFar);
+
+    if(!req.body.letter) {
+        res.status(422);
+        res.send({ error: "User must provide a letter"});
+    } else if (req.body.letter > 1) {
+        res.status(422);
+        res.send({ error: "User must provide letter only"});
+    } else if (req.body.letter > 1) {
+        res.status(422);
+        res.send({ error: "User must provide letter only"});
+    } else {
+        guessedLetters[req.body.letter] = true;
+        res.status(200);
+        res.send();
+    }
+});
+
+
+
+server.listen(PORT, err => {
+    if(err) {
+        console.log("There was an error");
+    } else {
+        console.log(`Server is listening on PORT number ${PORT}`);
+    }
+});
